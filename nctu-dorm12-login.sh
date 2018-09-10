926eb4a0ced0a6e70ffb52d43c6f6a29afc69093
xdavidwu <xdavidwuph@gmail.com>
2018-09-10 08:39:15 +0800

    login: fix logic

--- nctu-dorm12-login.sh
+++ nctu-dorm12-login.sh
@@ -7,7 +7,7 @@ if [ ! -n "$2" ];then
 	exit 1
 fi
 
-if [ ! -n "$(curl -d "username=$1&userpwd=$2" -X POST -v 'http://140.113.0.38/cgi-bin/ace_web_auth.cgi?web_jumpto=&orig_referer=http://detectportal.firefox.com/success.txt'| grep -F '/login_online_detail.php?show_portal=1&orig_referer=http://detectportal.firefox.com/success.txt&login_page=')" ];then
+if [ -n "$(curl -d "username=$1&userpwd=$2" -X POST -v 'http://140.113.0.38/cgi-bin/ace_web_auth.cgi?web_jumpto=&orig_referer=http://detectportal.firefox.com/success.txt'| grep -F '/login_online_detail.php?show_portal=1&orig_referer=http://detectportal.firefox.com/success.txt&login_page=')" ];then
 	echo 'Success'
 else
 	echo 'Failed'
