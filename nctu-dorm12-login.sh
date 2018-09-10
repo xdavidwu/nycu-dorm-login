f0de2689b827627048449dccb2e8802a3e140c00
xdavidwu <xdavidwuph@gmail.com>
2018-09-10 20:19:14 +0800

    login: make it more secure

--- nctu-dorm12-login.sh
+++ nctu-dorm12-login.sh
@@ -1,13 +1,15 @@
 #!/bin/sh
 
-if [ ! -n "$2" ];then
-	echo "Usage: $0 ACCOUNT PASSWORD"
-	echo "Login to NCTU dorm network with ACCOUNT and PASSWORD."
-	echo "Note: Password is sent in cleartext"
-	exit 1
-fi
+printf "Account: "
+read ACCOUNT
 
-if [ -n "$(curl -d "username=$1&userpwd=$2" -X POST -v 'http://140.113.0.38/cgi-bin/ace_web_auth.cgi?web_jumpto=&orig_referer=http://detectportal.firefox.com/success.txt'| grep -F '/login_online_detail.php?show_portal=1&orig_referer=http://detectportal.firefox.com/success.txt&login_page=')" ];then
+stty -echo
+printf "Password: "
+read PASSWD
+stty echo
+echo
+
+if [ -n "$(curl -d "username=$ACCOUNT&userpwd=$PASSWD" -v 'http://140.113.0.38/cgi-bin/ace_web_auth.cgi?web_jumpto=&orig_referer=http://detectportal.firefox.com/success.txt' 2>&1 | grep -F '/login_online_detail.php?show_portal=1&orig_referer=http://detectportal.firefox.com/success.txt&login_page=')" ];then
 	echo 'Success'
 else
 	echo 'Failed'
