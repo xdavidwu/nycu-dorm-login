5250edd1b6be867032efcfb68cc9ee393a0941c5
C.W. Chien <dppss92132@gmail.com>
2021-02-03 16:07:09 +0800

    fix domain name for new school name NYCU

--- nctu-dorm-login.sh
+++ nctu-dorm-login.sh
@@ -9,7 +9,7 @@ read PASSWD
 stty echo
 echo
 
-if [ -n "$(curl -d "username=$ACCOUNT&userpwd=$PASSWD" -v 'https://dorm.nctu.edu.tw/cgi-bin/ace_web_auth.cgi?web_jumpto=&orig_referer=http://detectportal.firefox.com/success.txt' 2>&1 | grep -F '/login_online_detail.php?show_portal=1')" ];then
+if [ -n "$(curl -d "username=$ACCOUNT&userpwd=$PASSWD" -v 'https://dorm.nycu.edu.tw/cgi-bin/ace_web_auth.cgi?web_jumpto=&orig_referer=http://detectportal.firefox.com/success.txt' 2>&1 | grep -F '/login_online_detail.php?show_portal=1')" ];then
 	echo 'Success'
 else
 	echo 'Failed'
